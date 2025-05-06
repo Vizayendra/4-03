14cf4392dee2b7b449f95f332796f83353cdfc50
Akshathaa <akshathaa1404@gmaill.com>
2025-05-06 10:38:55 +0800

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__)
+
+# Temporary storage for online meetings
+meetings = [
+    {
+        'title': 'Math Revision Group',
+        'time': '2025-05-10 5:00 PM',
+        'link': 'https://zoom.us/j/123456789'
+    },
+    {
+        'title': 'Science Study Session',
+        'time': '2025-05-12 4:30 PM',
+        'link': 'https://meet.google.com/xyz-abc-def'
+    }
+]
